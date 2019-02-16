792e9bec584150ebad7502ca300efa9d94878037
pfeitor <paulo_feitor@yahoo.com.br>
2019-02-16 20:03:47 -0200

    Adicionando UsuarioConfiguration

--- 2 - Infra/2.2 - Data/Switch.Infra.Data/Context/SwitchContext.cs
+++ 2 - Infra/2.2 - Data/Switch.Infra.Data/Context/SwitchContext.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Switch.Domain.Entities;
+using Switch.Infra.Data.Config;
+
 namespace Switch.Infra.Data.Context
 {
     public class SwitchContext : DbContext
@@ -8,8 +10,12 @@ namespace Switch.Infra.Data.Context
 
         public SwitchContext(DbContextOptions options):base(options)
         {
+        }
 
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguration());
+            base.OnModelCreating(modelBuilder);
         }
 
     }
