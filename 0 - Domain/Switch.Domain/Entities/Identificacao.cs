929e5bd3be9d33eaca9f62203ed874031a5cd4af
pfeitor <paulo_feitor@yahoo.com.br>
2019-02-16 21:56:41 -0200

    Adicionado classe identificacao

---
+++ 0 - Domain/Switch.Domain/Entities/Identificacao.cs
@@ -0,0 +1,13 @@
+using Switch.Domain.Enums;
+
+namespace Switch.Domain.Entities
+{
+    public class Identificacao
+    {
+        public int Id { get; set; }
+        public TipoDocumentoEnum  TipoDocumento { get; set; }
+        public string Numero { get; set; }
+        public int UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
+    }
+}
---
+++ 0 - Domain/Switch.Domain/Enums/TipoDocumento.cs
@@ -0,0 +1,9 @@
+namespace Switch.Domain.Enums
+{
+    public enum TipoDocumentoEnum
+    {
+        NaoDefinido = 0,
+        CPF = 1,
+        RG = 2
+    }
+}
